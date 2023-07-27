6d5ee7123c7419d178fb38606e34a8e5c3463561
YuriBocharov <quesadillaman@gmail.com>
2023-07-26 20:34:36 -0400

    wip

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -8,6 +8,9 @@
   };
 
   home.file.".tmux.conf".source = ./config;
+  home.file.".config/tmux/statusline.conf".source = ./statusline.config;
+  home.file.".config/tmux/tmux-cht-command".source = ./tmux-cht-command;
+  home.file.".config/tmux/tmux-cht-languages".source = ./tmux-cht-languages;
   home.file.".config/tmux/scripts" = {
     source = ./scripts;
     # copy the scripts directory recursively
