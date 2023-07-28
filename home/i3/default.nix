9ba71dda241faa302604c47c8b2c95d7c1bd0ca9
YuriBocharov <quesadillaman@gmail.com>
2023-07-28 01:25:00 -0400

    wip

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -7,14 +7,11 @@
   # wallpaper, binary file
   home.file = {
     ".config/i3/config".source = ./config;
-    ".config/i3/scripts/tmux-session.sh".source = ./scripts/tmux-session.sh;
-    ".config/i3/scripts/tmux-session.sh".executable = true;
 
-    # ".config/i3/scripts" = {
-      # source = ./scripts;
-      # # copy the scripts directory recursively
-      # recursive = true;
-      # executable = true; # make all scripts executable
-    # };
+    ".config/i3/scripts" = {
+      source = ./scripts;
+      # copy the scripts directory recursively
+      recursive = true;
+    };
   };
 }
--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -18,21 +18,12 @@
     ".config/tmux/tmux-cht-command".source = ./tmux-cht-command;
     ".config/tmux/tmux-cht-languages".source = ./tmux-cht-languages;
 
-    ".config/tmux/scripts/tmux-sessionizer".source = ./scripts/tmux-sessionizer;
-    ".config/tmux/scripts/tmux-sessionizer".executable = true;
-
-    ".config/tmux/scripts/tmux-windowizer".source = ./scripts/tmux-windowizer;
-    ".config/tmux/scripts/tmux-windowizer".executable = true;
-
-    ".config/tmux/scripts/tmux-cht.sh".source = ./scripts/tmux-cht.sh;
-    ".config/tmux/scripts/tmux-cht.sh".executable = true;
+    ".config/tmux/scripts" = {
+      source = ./scripts;
+      # copy the scripts directory recursively
+      recursive = true;
+    };
   };
 
 
-  # home.file.".config/tmux/scripts" = {
-    # source = ./scripts;
-    # # copy the scripts directory recursively
-    # recursive = true;
-    # executable = true;  # make all scripts executable
-  # };
 }
