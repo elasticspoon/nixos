8af2cb8ecc3cc853866704a42fc663823071c2c9
YuriBocharov <quesadillaman@gmail.com>
2023-07-28 00:04:34 -0400

    wip

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -11,10 +11,18 @@
     # ".config/i3/scripts/tmux-session.sh".executable = true;
 
     ".config/i3/scripts" = {
-      source = ./scripts;
-# copy the scripts directory recursively
+      # source = ./scripts;
       recursive = true;
       executable = true;  # make all scripts executable
+      text = ''
+#!/usr/bin/env bash
+
+# Get the name of the current i3 workspace
+# WORKSPACE_NAME=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
+WORKSPACE_NAME=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
+SESSION_NAME="terminal-$WORKSPACE_NAME"
+TERMINAL_DATA=$(i3-msg -t get_tree | jq '.. | select(.name? | tostring | startswith("'"$SESSION_NAME"'"))')
+      ''
     };
   };
 
