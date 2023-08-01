116ba380d4b740762af9990e76bcb5f9f9980e66
YuriBocharov <quesadillaman@gmail.com>
2023-08-01 19:54:02 -0400

    wallpaper

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -2,6 +2,8 @@
   # wallpaper, binary file
   home.file = {
     ".config/i3/config".source = ./config;
+    ".config/i3/wallpaper-l".source = ./wallpaper-l;
+    ".config/i3/wallpaper-r".source = ./wallpaper-r;
 
     ".config/i3/scripts" = {
       source = ./scripts;
