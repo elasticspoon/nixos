3583afe3b111cffc9333caaacc1c8686c1bcdc15
YuriBocharov <quesadillaman@gmail.com>
2023-11-12 19:09:33 -0500

    syncthing updates
    wip
    
    wip

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -12,6 +12,10 @@
 
       settings = {
         devices = {
+          "laptop" = {
+            id = "A6CVZED-CXVT737-D2ZS7GQ-6D3HIF4-2J6LQNQ-SZL3LRQ-VOQSF4V-W4EB2AA";
+            name = "NixOS Laptop";
+          };
           "Pixel 6a" = {
             id = "EO5PL2Y-WNWNWAM-4QGGDVB-5A47DHC-SJKOZVP-KSG4DLE-JXKIJDY-C2TJ3QS";
             name = "Pixel 6a";
@@ -26,24 +30,28 @@
           "Ubuntu VM" = {
             id = "MDEBSUS-6CJPFZY-32PZKO2-TMKG6BD-K4JAORO-SFE3PVV-47MXES2-SJ3IEAS";
           };
+          "desktop" = {
+            id = "YYLQEUI-MMEPEY3-HEP2E3A-JRUVVBF-QJRAWWI-QF3CKKF-HVZG6IN-HTXAUAV";
+            name = "NixOS Desktop";
+          };
         };
 
         folders = {
           "Misc" = {
             id = "a2bre-qfgy9";
-            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" ];
+            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" "laptop" "desktop" ];
             path = "/home/${user}/Sync/Misc";
             label = "Misc Shared";
           };
           "Obsidian" = {
             id = "s00ud-8co1w";
-            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" ];
+            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" "laptop" "desktop" ];
             path = "/home/${user}/Sync/Notes";
             label = "Obsidian";
           };
           "PDF" = {
             id = "ta3z3-6zyk0";
-            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" ];
+            devices = [ "Pixel 6a" "Windows Desktop" "Yuri Tablet" "Ubuntu VM" "laptop" "desktop" ];
             path = "/home/${user}/Sync/PDF";
             label = "PDF Notes";
           };
